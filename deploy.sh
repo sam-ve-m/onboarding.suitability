a783fd49b2b592082ddaaebf5c89d7924fd278f0
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-09 14:50:09 -0300

    chore: updated image version to 0.2.0-0 in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name onb-br-suitability-env --image nexus.sigame.com.br/fission-onboarding-br-suitability:0.1.1 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
+fission env create --spec --name onb-br-suitability-env --image nexus.sigame.com.br/fission-onboarding-br-suitability::0.2.0-0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
 fission fn create --spec --name onb-br-suitability-fn --env onb-br-suitability-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
 fission route create --spec --name onb-br-suitability-rt --method POST --url /onboarding/set_suitability --function onb-br-suitability-fn
